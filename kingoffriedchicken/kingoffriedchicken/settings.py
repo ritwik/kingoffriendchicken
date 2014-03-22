5b1e606ae2f3aa53e8db5a7f0a55eb73adbbff60
Ritwik Roy <rana.roy@gmail.com>
2014-03-23 00:59:46 +1100

    Website app and migrations

--- kingoffriedchicken/kingoffriedchicken/settings.py
+++ kingoffriedchicken/kingoffriedchicken/settings.py
@@ -115,7 +115,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Thirdparty
     'south',
+    # Local.
+    'website',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
